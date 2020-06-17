868c6a38773530c6461971276532c8cd84900d8a
Chris Ferguson <cfergus7@gmail.com>
2020-06-17 01:13:31 -0500

    got my js outlines

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(school){
+        // maybe this look into it
+        const name = name;
+        const id = id;
+        const email = email;
+
+        super(name, id, email);
+        this.school = school;
+
+    }
+
+    getRole(){
+        
+    }
+
+    getSchool(){
+        
+    }
+
+}
